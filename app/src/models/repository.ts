desktop/desktop

Add categories storage and Repository.categoryId
Introduces the persistence layer for sidebar repository categories
(issue 01 slice 1).

* Bumps the Dexie schema to v10 with a new `categories` table and an
  additive `categoryId` index on `repositories`. The migration is
  purely additive, so existing rows load as uncategorized.
* Adds a `Category` model and a `CategoriesStore` (getAll + create with
  case-insensitive uniqueness) that mirrors `RepositoriesStore`.
* Threads `categoryId` through the `Repository` model, its equality
  hash, and every `toRepository` / store update site so the field
  survives round-trips and re-emits.
* Re-exports `CategoriesStore` from the stores barrel.

Co-Authored-By: Claude Opus 4.7 (1M context) <noreply@anthropic.com>

app/src/models/repository.ts
@@ -64,7 +64,12 @@ export class Repository {
      * hasn't been resolved yet (e.g. for repositories added before this
      * property was introduced).
      */
-    public readonly gitDir: string | undefined = undefined
+    public readonly gitDir: string | undefined = undefined,
+    /**
+     * The id of the user-defined category this repository is assigned to, or
+     * null when uncategorized.
+     */
+    public readonly categoryId: number | null = null
   ) {
     this.mainWorkTree = { path }
     this.name = (gitHubRepository && gitHubRepository.name) || getBaseName(path)
@@ -76,7 +81,8 @@ export class Repository {
       this.missing,
       this.alias,
       this.workflowPreferences.forkContributionTarget,
-      this.isTutorialRepository
+      this.isTutorialRepository,
+      this.categoryId
     )
   }
 

diegorv/github-desktop-personal-version

app/src/models/repository.ts
@@ -65,7 +65,12 @@ export class Repository {
      * it, so the worktree set is not always discoverable after the fact. This
      * records the main worktree while it is still known.
      */
-    public readonly mainWorktreePath: string | undefined = undefined
+    public readonly mainWorktreePath: string | undefined = undefined,
+    /**
+     * The id of the user-defined category this repository is assigned to, or
+     * null when uncategorized.
+     */
+    public readonly categoryId: number | null = null
   ) {
     this.name = (gitHubRepository && gitHubRepository.name) || getBaseName(path)
 
@@ -76,7 +81,8 @@ export class Repository {
       this.missing,
       this.alias,
       this.workflowPreferences.forkContributionTarget,
-      this.isTutorialRepository
+      this.isTutorialRepository,
+      this.categoryId
     )
   }
 